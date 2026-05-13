e2e3ca7b6d101927b1b02b0094c03f407f442288
André Luiz <andreluizsa00@gmail.com>
2026-05-13 11:15:14 -0300

    fix(nuxt): change nitro preset to use environment variable or default to node-server

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -49,7 +49,7 @@ export default defineNuxtConfig({
     redirect: false,
   },
   nitro: {
-    preset: 'netlify',
+    preset: process.env.NITRO_PRESET ?? 'node-server',
   },
   i18n: {
     defaultLocale: parishConfig.i18n.default_locale,
